557fcd60456f75853abece1cac6d08037f56961d
traubms <mtraub@mit.edu>
2016-07-09 23:35:11 -0400

    improved rush schedule

--- draft/js/main.js
+++ draft/js/main.js
@@ -217,32 +217,32 @@ $(".play-video").on("click",function(e) {
     }, 1000);
 });
 
-/* ================================================
-   Magnific Popup
-   ================================================ */
-if ($('.popup-gallery').length) {
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        },
-        zoom: {
-            enabled: true,
-            duration: 300, // don't foget to change the duration also in CSS
-            opener: function(element) {
-                return element.find('img');
-            }
-        }
-    });
-}
+// /* ================================================
+//    Magnific Popup
+//    ================================================ */
+// if ($('.popup-gallery').length) {
+//     $('.popup-gallery').magnificPopup({
+//         delegate: 'a',
+//         type: 'image',
+//         tLoading: 'Loading image #%curr%...',
+//         mainClass: 'mfp-img-mobile',
+//         gallery: {
+//             enabled: true,
+//             navigateByImgClick: true,
+//             preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+//         },
+//         image: {
+//             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+//         },
+//         zoom: {
+//             enabled: true,
+//             duration: 300, // don't foget to change the duration also in CSS
+//             opener: function(element) {
+//                 return element.find('img');
+//             }
+//         }
+//     });
+// }
 
 /* ================================================
    jQuery Validate - Reset Defaults
