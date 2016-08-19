666624118bd276fed0bde7d50fa6715cba936cc1
traubms <mtraub@mit.edu>
2016-08-18 21:31:20 -0400

    some d3 stuff

--- draft/edit_brothers.py
+++ draft/edit_brothers.py
@@ -2,6 +2,37 @@ import json
 import pandas as pd
 import datetime
 
+
+from urllib2 import Request, urlopen, URLError
+
+def query_location(where):
+	formatted = "+".join(where.split(" "))
+	query = "http://maps.googleapis.com/maps/api/geocode/json?address=" + formatted + "&sensor=false"
+	request = Request(query)
+	try:
+		response = urlopen(request)
+		data = eval(response.read())['results'][0]
+		lat_lng = data['geometry']['location']
+		full_address_short_long = [(d['types'][0], d['short_name'], d['long_name']) for d in data['address_components']]
+		full_address_short_long.reverse()
+		address_type, full_address_short, full_address_long = zip(*full_address_short_long)
+		value_dict = dict([(x[0], x[1:]) for x in full_address_short_long])
+		if value_dict['country'][0] == 'US':
+			use_values = ['country','administrative_area_level_1', 'locality']
+		elif 'locality' in value_dict:
+			use_values = ['country', 'locality']
+		else:
+			use_values = ['country']
+		try:
+			full_address_short = ".".join([value_dict[x][0] for x in use_values])
+			full_address_long = ".".join([value_dict[x][1] for x in use_values])
+		except:
+			full_address_short = None
+			full_address_short = None
+		return (lat_lng['lat'], lat_lng['lng']), full_address_long, full_address_short, data
+	except URLError, e:
+	    print 'No kittez. Got an error code:', e
+
 path_to_brothers = ""
 brothers_json_file = "brothers.json"
 
@@ -14,6 +45,7 @@ def save(data):
 	data[['year', 'pledge no']] = data[['year', 'pledge no']].astype(int)
 	data = data.sort_values(by = ['year', 'pledge no'], ascending=[True, True])
 	data = add_class_labels(data)
+	data = validate_locations(data)
 	data.to_json(path_to_brothers + brothers_json_file, orient="records")
 
 def add_bro():
@@ -35,7 +67,39 @@ def parse_value(col, value):
 		value = int(value)
 	elif col in ['year matches graduation']:
 		value = value.lower() in ['y', 'yes', 'true']
+	elif col in ['sports', 'internships', 'courses', 'clubs']:
+		print value
+		value = str(value)
+		if not value.startswith("["):
+			value = '[' + value
+		if not value.endswith("]"):
+			value = value + ']'
+		print value
+		value = eval(value)
+		print value
+		value = [str(v) for v in value]
 	return value
+
+def validate_locations(data):
+	lon_lats = []
+	long_names = []
+	short_names = []
+	for town in data['hometown']:
+		try:
+			lon_lat, long_name, short_name, full = query_location(town)
+		except:
+			print "Hometown %s was unsuccessfully queried" % town
+			lon_lat = None
+			long_name = ""
+			short_name = ""
+		lon_lats += [lon_lat]
+		long_names += [long_name]
+		short_names += [short_name]
+	data['coords'] = lon_lats
+	data['hometown_long'] = long_names
+	data['hometown_short'] = short_names
+	return data
+
 
 def search_select(names):
 	inp = "***"
@@ -86,11 +150,19 @@ def edit_bro():
 					print
 					inp = raw_input("Select column to modify: ")
 					if inp in bro:
-						val = raw_input("New value: ")
-						if val != "":
-							data[inp][where_bro] = parse_value(inp, val)
-						else:
-							print "Update aborted."
+						try_again = True
+						while try_again:
+							try_again = False
+							val = raw_input("New value: ")
+							if val != "":
+								try:
+									index = data[where_bro].index[0]
+									data.set_value(index, inp, parse_value(inp, val))
+								except:
+									print "Parsing failed. Try again."
+									try_again = True
+							else:
+								print "Update aborted."
 					elif inp != "":
 						print "Invalid column input."
 				save(data)
