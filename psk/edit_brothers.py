cecf65ecf5845d054723efc6105fa2304401bcc5
traubms <mtraub@mit.edu>
2017-06-23 23:29:00 -0400

    python3 compatible?

--- psk/edit_brothers.py
+++ psk/edit_brothers.py
@@ -1,7 +1,9 @@
+from __future__ import print_function
 import json
 import pandas as pd
 import datetime
 import time
+
 
 
 from urllib2 import Request, urlopen, URLError
@@ -31,8 +33,8 @@ def query_location(where):
 			full_address_short = None
 			full_address_short = None
 		return (lat_lng['lat'], lat_lng['lng']), full_address_long, full_address_short, data
-	except URLError, e:
-	    print 'No kittez. Got an error code:', e
+	except URLError as e:
+	    print('No kittez. Got an error code:', e)
 
 path_to_brothers = "data/"
 brothers_json_file = "brothers.json"
@@ -69,15 +71,15 @@ def parse_value(col, value):
 	elif col in ['year matches graduation']:
 		value = value.lower() in ['y', 'yes', 'true']
 	elif col in ['sports', 'internships', 'courses', 'clubs']:
-		print value
+		print(value)
 		value = str(value)
 		if not value.startswith("["):
 			value = '[' + value
 		if not value.endswith("]"):
 			value = value + ']'
-		print value
+		print(value)
 		value = eval(value)
-		print value
+		print(value)
 		value = [str(v) for v in value]
 	return value
 
@@ -90,11 +92,12 @@ def validate_locations(data):
 		lon_lat = data['coords'][index]
 		long_name = data['hometown_long'][index]
 		short_name = data['hometown_short'][index]
-		try:
-			lon_lat, long_name, short_name, full = query_location(town)
-			success = True
-		except:
-			print "Hometown %s was unsuccessfully queried" % town
+		if lon_lat is None or long_name is None or short_name is None:
+			try:
+				lon_lat, long_name, short_name, full = query_location(town)
+				success = True
+			except:
+				print("Hometown %s was unsuccessfully queried" % town)
 		lon_lats += [lon_lat]
 		long_names += [long_name]
 		short_names += [short_name]
@@ -109,24 +112,24 @@ def search_select(names):
 	inp = "***"
 	while inp != "":
 		for n in names:
-			print "\t", n
+			print("\t", n)
 		names_lower = names.apply(lambda x: x.lower()).values
 		inp = raw_input("Select a brother: ").lower()
 		where_still = pd.Series(names_lower).apply(lambda n: inp in n)
 		n_left = where_still.sum()
-		print n_left
+		print(n_left)
 		if n_left == 0:
-			print "No options contain '%s'." % inp
+			print("No options contain '%s'." % inp)
 		elif n_left >= 1:
 			if n_left == 1:
-				print names
-				print where_still
+				print(names)
+				print(where_still)
 				name = names[where_still.values].iloc[0]
 				break
 			else:
 				names = names[where_still.values].copy()
-				print names
-				print "%s options left:" % n_left
+				print(names)
+				print("%s options left:" % n_left)
 	if inp == "":
 		return inp
 	else:
@@ -139,10 +142,10 @@ def edit_bro():
 		names = data['name']
 	else:
 		names = data[data['in school'].astype(bool)]['name']
-	print "Availible Bros:"
+	print("Availible Bros:")
 	if len(names) > 0:
 		inp = search_select(names)
-		print inp
+		print(inp)
 		if inp != "":
 			name = inp
 			where_bro = data['name'] == name
@@ -150,8 +153,8 @@ def edit_bro():
 				inp = "***"
 				while inp != "":
 					bro = data[where_bro]
-					print "\nSelected Brother:\n", bro.transpose()
-					print
+					print("\nSelected Brother:\n", bro.transpose())
+					print()
 					inp = raw_input("Select column to modify: ")
 					if inp in bro:
 						try_again = True
@@ -163,39 +166,39 @@ def edit_bro():
 									index = data[where_bro].index[0]
 									data.set_value(index, inp, parse_value(inp, val))
 								except:
-									print "Parsing failed. Try again."
+									print("Parsing failed. Try again.")
 									try_again = True
 							else:
-								print "Update aborted."
+								print("Update aborted.")
 					elif inp != "":
-						print "Invalid column input."
+						print("Invalid column input.")
 				save(data)
 			else:
-				print "Edit aborted."
+				print("Edit aborted.")
 	else:
-		print "No availible brothers."
+		print("No availible brothers.")
 
 def delete_bro():
 	data = load()
 	inp = search_select(data['name'])
-	print inp
+	print(inp)
 	bro = data[data['name'] == inp]
 
 	if inp != "":
-		print "\nSelected Brother:\n", bro.replace("\n", "\t\n")
+		print("\nSelected Brother:\n", bro.replace("\n", "\t\n"))
 
 		sure = raw_input("Are you sure you want to delete?").lower()
 		if sure in ['y', 'yes', 'true']:
 			data = data[data['name'] != inp]
 			save(data)
 		else:
-			print "Delete aborted."
+			print("Delete aborted.")
 
 
 
 def view_bros():
-	print load()
-	print 
+	print(load())
+	print()
 	
 
 def get_class_mapping():
@@ -222,12 +225,12 @@ def add_class_labels(data):
 def main():
 	inp = "**"
 	while inp != "q":
-		print "Options:"
-		print "1. Add brother"
-		print "2. Edit brother"
-		print "3. Delete brother"
-		print "4. View data"
-		print "Q. Quit"
+		print("Options:")
+		print("1. Add brother")
+		print("2. Edit brother")
+		print("3. Delete brother")
+		print("4. View data")
+		print("Q. Quit")
 		inp = raw_input("Select option (Q to quit): ").lower()
 		if inp == "1":
 			add_bro()
@@ -238,7 +241,7 @@ def main():
 		elif inp == "4":
 			view_bros()
 
-	print "Goodbye!"
+	print("Goodbye!")
 
 if __name__ == "__main__":
 	main()
